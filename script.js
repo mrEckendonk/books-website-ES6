c864bc42c3af8bb169ffd86e76bfef6b42c214bc
Mike van Eckendonk <mike.e@vecks.com.br>
2022-07-15 19:49:15 -0300

    Not working Links Fix path - 1

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-import { DateTime } from '/node_modules/luxon/src/luxon.js';
-import controlEvents from '/modules/navigation.js';
-import Navigation  from "/modules/nav_bar.js";
+import { DateTime } from './node_modules/luxon/src/luxon.js';
+import controlEvents from './modules/navigation.js';
+import Navigation  from "./modules/nav_bar.js";
 
 const nav = new Navigation();
 nav.view();
